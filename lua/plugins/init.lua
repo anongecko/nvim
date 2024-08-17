5bd3bebfc017b59685d14ade619df6f7c83c4d5f
bytes <anongecko@tutamail.com>
2024-08-17 10:02:53 -0400

    Added cheatsheet plugin. :D

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -51,6 +51,18 @@ return {
     end,
   },
 
+  {
+    "doctorfree/cheatsheet.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      { "nvim-telescope/telescope.nvim" },
+      { "nvim-lua/popup.nvim" },
+      { "nvim-lua/plenary.nvim" },
+    },
+    config = function()
+      require "configs.cheatsheet"
+    end,
+  },
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
